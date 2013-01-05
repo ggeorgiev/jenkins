76fc0ef88795c6e390305ad8fc4fbc2f8df8eab9
george <george.georgiev@hotmail.com>
2013-01-05 11:39:55 -0800

    fix boost linux script

--- boost/linux/build.sh
+++ boost/linux/build.sh
@@ -12,7 +12,7 @@ popd || exit 1
 
 ./b2 variant=debug address-model=32 architecture=x86 --stagedir=./prebuilt/lib/linux/x86-32/debug --with-system --with-filesystem --with-program_options --with-regex || exit 1
 mv ./prebuilt/lib/linux/x86-32/debug/lib/* ./prebuilt/lib/linux/x86-32/debug || exit 1
-rmdir ./prebuilt/lib/macosx/x86-32/debug/lib || exit 1
+rmdir ./prebuilt/lib/linux/x86-32/debug/lib || exit 1
 
 ./b2 variant=release address-model=32 architecture=x86 --stagedir=./prebuilt/lib/linux/x86-32/release --with-system --with-filesystem --with-program_options --with-regex || exit 1
 mv ./prebuilt/lib/linux/x86-32/release/lib/* ./prebuilt/lib/linux/x86-32/release || exit 1
