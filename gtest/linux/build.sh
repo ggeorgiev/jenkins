fabb7e0706c6788f40c72fa21b3b7cb3f3e59f10
george <george.georgiev@hotmail.com>
2013-01-05 18:47:04 -0800

    fix gtest linux build script

--- gtest/linux/build.sh
+++ gtest/linux/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-g++ -g -march=i386 -m64 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -g -m64 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/debug || exit 1
 rm ./gtest-all.o
 
-g++ -march=i386 -m64 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -m64 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/release || exit 1
 rm ./gtest-all.o
