fa8f5686297bdc74bb99249c0b06d4ec0afc1c01
george <george.georgiev@hotmail.com>
2013-01-06 12:32:54 -0800

    use i486 for 32 builds on linux

--- boost/linux/build.sh
+++ boost/linux/build.sh
@@ -6,11 +6,11 @@ pushd tools/build/v2 || exit 1
 popd || exit 1
 
 ./bootstrap.sh || exit 1
-./b2 variant=debug address-model=32 architecture=x86 --stagedir=./prebuilt/lib/linux/x86-32/debug --with-system --with-filesystem --with-program_options --with-regex || exit 1
+./b2 variant=debug address-model=32 architecture=x86 instruction-set=i486 --stagedir=./prebuilt/lib/linux/x86-32/debug --with-system --with-filesystem --with-program_options --with-regex || exit 1
 mv ./prebuilt/lib/linux/x86-32/debug/lib/* ./prebuilt/lib/linux/x86-32/debug || exit 1
 rmdir ./prebuilt/lib/linux/x86-32/debug/lib || exit 1
 
-./b2 variant=release address-model=32 architecture=x86 --stagedir=./prebuilt/lib/linux/x86-32/release --with-system --with-filesystem --with-program_options --with-regex || exit 1
+./b2 variant=release address-model=32 architecture=x86  instruction-set=i486  --stagedir=./prebuilt/lib/linux/x86-32/release --with-system --with-filesystem --with-program_options --with-regex || exit 1
 mv ./prebuilt/lib/linux/x86-32/release/lib/* ./prebuilt/lib/linux/x86-32/release || exit 1
 rmdir ./prebuilt/lib/linux/x86-32/release/lib || exit 1
 
--- gtest/linux/build.sh
+++ gtest/linux/build.sh
@@ -12,13 +12,13 @@ mkdir -p ./prebuilt/lib/linux/x86-64/release || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/release || exit 1
 rm ./gtest-all.o
 
-g++ -g -march=i386 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -g -march=i486 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mkdir -p ./prebuilt/lib/linux/x86-32/debug || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-32/debug || exit 1
 rm ./gtest-all.o
 
-g++ -march=i386 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -march=i486 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mkdir -p ./prebuilt/lib/linux/x86-32/release || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-32/release || exit 1
