419abe14fef1c04576a217a34e767919004ba205
george <george.georgiev@hotmail.com>
2013-01-05 18:41:41 -0800

    fix gtest linux build script

--- gtest/linux/build.sh
+++ gtest/linux/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-g++ -g -arch x86_64 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -g -march=x86_64 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/debug || exit 1
 rm ./gtest-all.o
 
-g++ -arch x86_64 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -march=x86_64 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/release || exit 1
 rm ./gtest-all.o
 
-g++ -g -arch i386 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -g -march=i386 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/debug || exit 1
 rm ./gtest-all.o
 
-g++ -arch i386 -Iinclude -I. -c src/gtest-all.cc || exit 1
+g++ -march=i386 -m32 -Iinclude -I. -c src/gtest-all.cc || exit 1
 ar -rv libgtest.a gtest-all.o || exit 1
 mv ./libgtest.a ./prebuilt/lib/linux/x86-64/release || exit 1
 rm ./gtest-all.o
